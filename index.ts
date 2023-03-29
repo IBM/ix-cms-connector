bad348285456bac580bf1de7892c33b5432c3f8e
Kevin Krug <kevin.krug@aperto.com>
2023-03-29 15:45:18 +0200

    FRONTEND-157. adds eslint. lint task. and types. TS refactor of schema generator task

---
+++ index.ts
@@ -0,0 +1,24 @@
+import process from 'process';
+import jsonSchemaGenerator from 'json-schema-generator';
+import axios from 'axios';
+
+const jsonToSchema = jsonSchemaGenerator,
+    obj = { some: { object: true } },
+    schemaObj;
+
+
+const testEndpoint = process.argv[2] || 'https://dummyjson.com/products/1';
+
+const getJson = async() => {
+    const res = await axios(testEndpoint);
+    return await res.data;
+}
+
+const getSchema = async() => {
+    const json = await getJson();
+    const schema = jsonToSchema(json);
+    console.log('schema', schema)
+}
+
+getSchema();
+
