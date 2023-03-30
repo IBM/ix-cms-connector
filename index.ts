7fdb1e94c91267bc85c676eb6468a9e98bf25708
Kevin Krug <kevin.krug@aperto.com>
2023-03-30 12:50:22 +0200

    FRONTEND-157. adds prettier

--- index.ts
+++ index.ts
@@ -1,24 +1,22 @@
-import process from 'process';
-import jsonSchemaGenerator from 'json-schema-generator';
-import axios from 'axios';
+import process from "process";
+import jsonSchemaGenerator from "json-schema-generator";
+import axios from "axios";
 
 const jsonToSchema = jsonSchemaGenerator,
-    obj = { some: { object: true } },
-    schemaObj;
+  obj = { some: { object: true } },
+  schemaObj;
 
+const testEndpoint = process.argv[2] || "https://dummyjson.com/products/1";
 
-const testEndpoint = process.argv[2] || 'https://dummyjson.com/products/1';
+const getJson = async () => {
+  const res = await axios(testEndpoint);
+  return await res.data;
+};
 
-const getJson = async() => {
-    const res = await axios(testEndpoint);
-    return await res.data;
-}
-
-const getSchema = async() => {
-    const json = await getJson();
-    const schema = jsonToSchema(json);
-    console.log('schema', schema)
-}
+const getSchema = async () => {
+  const json = await getJson();
+  const schema = jsonToSchema(json);
+  console.log("schema", schema);
+};
 
 getSchema();
-
