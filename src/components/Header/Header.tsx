95c5b2877514a7b54b6609c32a43e837f62a3c41
Sofia Puch <Sofia.Puch@ibmix.de>
2023-04-11 15:46:15 +0200

    FRONTEND-167 fix prettier

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,15 @@
 import { FunctionComponent } from "preact";
 
 /* Example of function component */
-const MainHeader: FunctionComponent<{title: string}> = ({title /*, children*/}) => {
+const MainHeader: FunctionComponent<{ title: string }> = ({
+  title /*, children*/,
+}) => {
   return (
     <header class="text-center py-2 bg-slate-200 mb-4">
       <h1 class="text-2xl">LIT</h1>
       <h2>{title}</h2>
     </header>
   );
-} ;
+};
 
 export default MainHeader;
