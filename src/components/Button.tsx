6664d6d51b3a60e1dca7c83e00d61545b5b9fad6
Kevin Krug <kevin.krug@aperto.com>
2023-04-13 16:37:18 +0200

    FRONTEND-162. adds button types

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,9 @@
-export const Button = ({text, ...rest}) => {
+import { HTMLAttributes } from "preact/compat";
+
+interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  text: string;
+}
+
+export const Button: FC<ButtonProps> = ({text, ...rest}) => {
   return <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" {...rest}>{text}</button>;
 };
