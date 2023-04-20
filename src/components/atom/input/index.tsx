add68f9361f2512944253875a0b1cecd4e638bbe
luma.kuehl <luma.kuehl@ibmix.de>
2023-04-20 10:27:14 +0200

    FRONTEND-202 otimize tailwind class

--- src/components/atom/input/index.tsx
+++ src/components/atom/input/index.tsx
@@ -10,7 +10,7 @@ export const Input: FunctionalComponent<InputProps> = ({ label, ...input }) => {
     <label class="text-text-02 font-normal text-xs flex flex-col">
       {label}
       <input
-        class="py-3.5 px-3.5 text-text-01 placeholder-text-03 bg-ui-shell-white border-solid border-b rounded-none border-ui-04 mb-2 mt-2 box-content focus:outline-focus focus:border-none"
+        class="p-3.5 text-text-01 placeholder-text-03 bg-ui-shell-white border-solid border-b rounded-none border-ui-04 my-2 box-content focus:outline-focus focus:border-none"
         {...input}
       ></input>
     </label>
