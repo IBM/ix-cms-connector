3a88c79c50f7a07d24580a0692fea9631b1b2e02
Sofia Puch <Sofia.Puch@ibmix.de>
2023-04-24 16:55:16 +0200

    FRONTEND-169 eslint

--- src/utils/funcs.test.ts
+++ src/utils/funcs.test.ts
@@ -1,7 +1,6 @@
 import { isPrimitiveType } from "./funcs";
 
 describe("utils/funcs", () => {
-
   describe("isPrimitiveType()", () => {
     it("should return 'true' if the given string matches a primitive type", () => {
       const typeString = "boolean";
