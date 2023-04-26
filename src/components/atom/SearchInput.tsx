390128afdf6520e23a6f2217460e47797c6c4a7d
luma.kuehl <luma.kuehl@ibmix.de>
2023-04-26 16:15:23 +0200

    FRONTEND-214 create basic overview page

--- src/components/atom/SearchInput.tsx
+++ src/components/atom/SearchInput.tsx
@@ -38,17 +38,17 @@ export const SearchInput: FunctionalComponent<SearchInputProps> = ({
       class="flex flex-col text-xs text-text-02 font-normal mb-2"
     >
       {label}
-      <div class="flex relative items-center bg-ui-shell-white focus:outline-8 border-b  border-ui-04">
+      <div class="flex relative items-center bg-ui-shell-white focus:outline-8 border-b  border-ui-04 mt-2">
         <Search class="absolute left-0 ml-3" />
         <input
           id="search"
           type="text"
-          value={searchTerm}
+          value={searchTerm}  
           aria-label={label}
           icon={Search}
           onChange={debouncedChange}
           {...input}
-          class="py-3.5 px-10 min-w-0 text-sm text-text-01 placeholder-text-03 bg-ui-shell-white box-border focus:outline-focus"
+          class="py-3.5 px-10 min-w-0 w-full text-sm text-text-01 placeholder-text-03 bg-ui-shell-white box-border focus:outline-focus"
         />
         {searchTerm && (
           <button
