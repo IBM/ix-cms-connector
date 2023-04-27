8690c121e87a5245208d5dfd6343261cab0903be
Sofia Puch <Sofia.Puch@ibmix.de>
2023-04-27 14:59:38 +0200

    FRONTEND-217 Mock module example on generate-schema

--- generate-schema.test.ts
+++ generate-schema.test.ts
@@ -1,10 +1,27 @@
+import { vi } from "vitest";
+
 import { getSchema } from "./generate-schema";
+import jsonSchemaGenerator from "json-schema-generator";
+
+// Mocks a module
+vi.mock("json-schema-generator");
+
+// Will be reused in the tests (it can be set individually)
+const testEndpoint = "https://dummyjson.com/products/1";
 
 describe("getSchema()", () => {
-  it("should execute the jsonSchemaGenerator method", () => {
-    const testEndpoint = "https://dummyjson.com/products/1";
+  it("should execute the jsonSchemaGenerator method", async () => {
+    await getSchema(testEndpoint);
 
-    // "return" and "resolve" is a way of handling async code
+    // Check that the mocked module was called
+    expect(jsonSchemaGenerator).toBeCalled();
+  });
+
+  it("should return the schema if an endpoint is provided", () => {
+    // resets the mocks, so we can use the real module
+    vi.restoreAllMocks();
+
+    // "return" and "resolve" is a way of handling a promise
     return expect(getSchema(testEndpoint)).resolves.toBeDefined();
   });
 });
