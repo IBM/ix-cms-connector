ffe319bbf5c1e9d45d99ccccfcf9533270e5fb54
Sofia Puch <Sofia.Puch@ibmix.de>
2023-04-27 09:37:58 +0200

    FRONTEND-217 One way to handle async code example

---
+++ generate-schema.test.ts
@@ -0,0 +1,11 @@
+import { getSchema } from "./generate-schema";
+
+describe("getSchema()", () => {
+  it("should execute the jsonSchemaGenerator method", () => {
+
+    const testEndpoint = "https://dummyjson.com/products/1";
+
+    // "return" and "resolve" is a way of handling async code
+    return expect(getSchema(testEndpoint)).resolves.toBeDefined();
+  });
+});
