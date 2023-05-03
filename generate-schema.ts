5908be8b0ce2899c77bfe7c89b23efae92f282b0
Ivan Karpov <ivan.karpov@aperto.com>
2023-05-03 12:47:10 +0200

    FRONTEND-225: remove obsolete generate-schema file

--- generate-schema.ts
+++
@@ -1,6 +0,0 @@
-import process from "process";
-import { getJSONSchema } from "src/utils";
-
-const testEndpoint = process.argv[2] || "https://dummyjson.com/products/1";
-
-getJSONSchema(testEndpoint);
