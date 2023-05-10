1b7e9ae55a39a74b3d966833452f644273afbe32
luma.kuehl <luma.kuehl@ibmix.de>
2023-05-10 10:48:37 +0200

    FRONTEND-252 add button  test

--- src/components/atoms/Button.test.tsx
+++ src/components/atoms/Button.test.tsx
@@ -2,11 +2,13 @@
 
 import { describe, it, expect, vi } from "vitest";
 import { render, screen, fireEvent } from "@testing-library/preact";
+
 import userEvent from "@testing-library/user-event";
 
 import { Button, ButtonType } from "./Button";
 
 const mockLabel = "Click here";
+const testId = "button";
 
 describe("Button", () => {
   it("should display the button in the dom", () => {
@@ -17,10 +19,60 @@ describe("Button", () => {
   });
 
   it("should display the button with the correct label", () => {
-    // add test here
+    render(<Button text={mockLabel} style={ButtonType.PRIMARY} />);
+
+    const buttonElement = screen.getByText(mockLabel);
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  it("should display the button with primary style", () => {
+    render(<Button text={mockLabel} style={ButtonType.PRIMARY} />);
+
+    const buttonElement = screen.getByTestId(testId);
+    expect(buttonElement).toHaveClass("bg-interactive-01");
+  });
+
+  it("should display the button with secondary style", () => {
+    render(<Button text={mockLabel} style={ButtonType.SECONDARY} />);
+
+    const buttonElement = screen.getByTestId(testId);
+    expect(buttonElement).toHaveClass("bg-interactive-02");
+  });
+
+  it("should display the button disabled with secondary style", () => {
+    const user = userEvent.setup();
+    const onClickSpy = vi.fn();
+
+    render(
+      <Button
+        text={mockLabel}
+        style={ButtonType.SECONDARY}
+        onClick={onClickSpy}
+        disabled
+      />
+    );
+
+    const buttonElement = screen.getByTestId(testId);   
+    user.click(buttonElement);
+
+    expect(onClickSpy).not.toBeCalled();
+    expect(buttonElement).toHaveClass("bg-interactive-02");
   });
 
   it("should call the onClick event", () => {
-    // add test here
+    const onClickSpy = vi.fn();
+
+    render(
+      <Button
+        text={mockLabel}
+        style={ButtonType.SECONDARY}
+        onClick={onClickSpy}
+      />
+    );
+
+    const buttonElement = screen.getByTestId(testId);
+    fireEvent.click(buttonElement);
+
+    expect(onClickSpy).toBeCalled();
   });
 });
