5992f8328c9620bd2141e50ea65cbe573469795d
Ivan Karpov <ivan.karpov@aperto.com>
2023-05-12 18:16:05 +0200

    FRONTEND-261: add tests for getComponentParserConfig

---
+++ src/utils/funcs/getComponentParserConfig.test.ts
@@ -0,0 +1,35 @@
+import { describe, it, expect } from "vitest";
+
+import { getComponentParserConfig } from "./getComponentParserConfig";
+
+describe("getComponentParserConfig()", () => {
+  it("should return proper config for parsing Typescript components if the given filename is a .ts file", () => {
+    const fileName = "file.ts";
+
+    const result = getComponentParserConfig(fileName);
+
+    expect(result).toEqual({
+      babelOptions: expect.objectContaining({
+        filename: fileName,
+        parserOpts: expect.objectContaining({
+          plugins: expect.arrayContaining(["typescript"]),
+        }),
+      }),
+    });
+  });
+
+  it("should return proper config for parsing JS components if the given filename is a .js file", () => {
+    const fileName = "file.js";
+
+    const result = getComponentParserConfig(fileName);
+
+    expect(result).toEqual({
+      babelOptions: expect.objectContaining({
+        filename: fileName,
+        parserOpts: expect.objectContaining({
+          plugins: expect.arrayContaining(["flow"]),
+        }),
+      }),
+    });
+  });
+});
