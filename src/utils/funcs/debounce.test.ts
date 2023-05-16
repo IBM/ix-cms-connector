b6be74cee7f26bb02f742fff94871fc5e483a6c6
luma.kuehl <luma.kuehl@ibmix.de>
2023-05-16 16:05:05 +0200

    FRONTEND-233 add debounce test

--- src/utils/funcs/debounce.test.ts
+++ src/utils/funcs/debounce.test.ts
@@ -1,8 +1,20 @@
 // @vitest-environment jsdom
 import { describe, it, expect, vi } from "vitest";
+import { debounce } from "./debounce";
 
 describe("debounce()", () => {
-  it("should wait for 3 seconds", () => {
-    //add test here
+  it("should wait for 3 seconds", async() => {
+    vi.useFakeTimers();
+    const fnSpy = vi.fn();
+
+    const debouncedFn = debounce(fnSpy, 300);
+    debouncedFn();
+
+    expect(fnSpy).not.toBeCalled();
+
+    vi.advanceTimersByTime(300);
+    vi.runAllTimers();
+
+    expect(fnSpy).toBeCalled();
   });
 });
