6264b65f5e5f88518dbf3b767466e85be1c90e68
Frank Robnik <frank.robnik@ibmix.de>
2023-05-16 12:03:04 +0200

    adding tests for propertyList

---
+++ src/components/molecules/PropertyList.test.tsx
@@ -0,0 +1,159 @@
+// @vitest-environment jsdom
+
+import { it, expect, describe, vi } from "vitest";
+
+import { render, screen, fireEvent } from "@testing-library/preact";
+
+import { PropertyList } from "./PropertyList";
+import { MappableProp, TSType } from "../../utils";
+import { Source } from "../atoms/Property";
+
+const testList: MappableProp[] = [
+  {
+    name: "string-test",
+    type: TSType.String,
+    isRequired: true,
+  },
+  {
+    name: "number-test",
+    type: TSType.Number,
+    isRequired: true,
+  },
+  {
+    name: "boolean-test",
+    type: TSType.Boolean,
+    isRequired: true,
+  },
+  {
+    name: "string-array-test",
+    type: TSType.Array,
+    subTypes: [TSType.String],
+    isRequired: true,
+  },
+  {
+    name: "union-test",
+    type: TSType.Union,
+    subTypes: [TSType.String, TSType.Undefined],
+    isRequired: false,
+  },
+];
+
+describe("PropertyList", () => {
+  it("should display a list of Properties", () => {
+    render(
+      <PropertyList
+        list={testList}
+        source={Source.CMS}
+        onPropertyDragStart={() => 0}
+        onPropertyClick={() => 0}
+        onDropOnProperty={() => 0}
+      />
+    );
+
+    const [outterListEl] = screen.getAllByRole("list");
+
+    expect(outterListEl).toBeInTheDocument();
+  });
+
+  it("should invoke the onPropertyDragStart handler when a property is dragged", () => {
+    const propertyListSpy = vi.fn();
+
+    render(
+      <PropertyList
+        list={testList}
+        source={Source.CMS}
+        onPropertyDragStart={propertyListSpy}
+        onPropertyClick={() => 0}
+        onDropOnProperty={() => 0}
+      />
+    );
+
+    const stringPropertyEl =
+      screen.getByText("string-test").parentElement.parentElement;
+    fireEvent.dragStart(stringPropertyEl);
+
+    expect(propertyListSpy).toBeCalled();
+  });
+
+  it("should invoke the onPropertyClick handler when a property is clicked", () => {
+    const propertyListSpy = vi.fn();
+
+    render(
+      <PropertyList
+        list={testList}
+        source={Source.CMS}
+        onPropertyDragStart={() => 0}
+        onPropertyClick={propertyListSpy}
+        onDropOnProperty={() => 0}
+      />
+    );
+
+    const stringPropertyEl =
+      screen.getByText("string-test").parentElement.parentElement;
+    fireEvent.click(stringPropertyEl);
+
+    expect(propertyListSpy).toBeCalled();
+  });
+
+  it("should invoke the onDropOnProperty handler when a property with different source is dropped", () => {
+    const propertyListSpy = vi.fn();
+
+    const draggingProp = {
+      propData: {
+        name: "dragging-test",
+        type: TSType.String,
+        isRequired: true,
+      },
+      source: Source.COMPONENT, // different then list source
+    };
+
+    render(
+      <PropertyList
+        list={testList}
+        source={Source.CMS}
+        draggingProp={draggingProp}
+        onPropertyDragStart={() => 0}
+        onPropertyClick={() => 0}
+        onDropOnProperty={propertyListSpy}
+      />
+    );
+
+    const stringPropertyListEl =
+      screen.getByText("string-test").parentElement.parentElement.parentElement;
+
+    fireEvent.drop(stringPropertyListEl);
+
+    expect(propertyListSpy).toBeCalled();
+  });
+
+  it("should NOT invoke the onDropOnProperty handler when a property with same source is dropped", () => {
+    const propertyListSpy = vi.fn();
+
+    const draggingProp = {
+      propData: {
+        name: "dragging-test",
+        type: TSType.String,
+        isRequired: true,
+      },
+      source: Source.CMS, // same than list source
+    };
+
+    render(
+      <PropertyList
+        list={testList}
+        source={Source.CMS}
+        draggingProp={draggingProp}
+        onPropertyDragStart={() => 0}
+        onPropertyClick={() => 0}
+        onDropOnProperty={propertyListSpy}
+      />
+    );
+
+    const stringPropertyListEl =
+      screen.getByText("string-test").parentElement.parentElement.parentElement;
+
+    fireEvent.drop(stringPropertyListEl);
+
+    expect(propertyListSpy).not.toBeCalled();
+  });
+});
